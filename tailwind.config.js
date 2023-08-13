3cfff5e33777770ea8a50aa63293f36a96fc7131
@d-donne <doublyd@gmail.com>
2023-08-13 10:13:40 +0000

    finished layout and addded responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,25 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0px 4px 46px -3px hsl(217, 45%, 90%)",
+      },
+    },
     colors: {
-      'red': 'hsl(0, 78%, 62%)',
-      'cyan': 'hsl(180, 62%, 55%)',
-
+      transparent: "transparent",
+      current: "currentColor",
+      red: "hsl(0, 78%, 62%)",
+      cyan: "hsl(180, 62%, 55%)",
+      orange: "hsl(34, 97%, 64%)",
+      blue: "hsl(212, 86%, 64%)",
+      "font-accent": "hsl(234, 12%, 34%)",
+      "font-primary": "hsl(229, 6%, 66%)",
+      veryLightGray: "hsl(0, 0%, 98%)",
+      green: "green",
+    },
+    fontFamily: {
+      "ff-primary": ["Poppins", "sans-serif"],
     },
   },
   plugins: [],
